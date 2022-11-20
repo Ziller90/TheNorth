784edeeb7bb67658b11b941fb1f7b6045331a37a
Zdorovtsov Ilya <ilyazdorovtsov@gmail.com>
2022-11-20 19:21:49 +0300

    Inventory fixed, description added

--- Assets/Scripts/Inventory/InventoryUI.cs
+++ Assets/Scripts/Inventory/InventoryUI.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class InventoryUI : MonoBehaviour
 {
     public GameObject inventoryPanel;
+    public GameObject mobileIngameInterface;
     public void OpenInventory()
     {
         inventoryPanel.SetActive(true);
+        mobileIngameInterface.SetActive(false);
     }
     public void CloseInventory()
     {
         inventoryPanel.SetActive(false);
+        mobileIngameInterface.SetActive(true);
     }
 }
