14937091de63dc3e31773c8f3bed1220a4f0afc8
Zdorovtsov Ilya <ilyazdorovtsov@gmail.com>
2022-12-04 12:23:21 +0300

    Player spawn fixed, action manager edited

--- Assets/Scripts/ScenesManagement/GameSceneInitializer.cs
+++ Assets/Scripts/ScenesManagement/GameSceneInitializer.cs
@@ -7,11 +7,15 @@ using UnityEngine.Events;
 public class GameSceneInitializer : MonoBehaviour
 {
     [SerializeField] GameObject Humanoid;
-    [SerializeField] ActionManager playerActionManager;
-    [SerializeField] ControlManager playerControlManager;
+
+    ControlManager playerControlManager;
+    ActionManager playerActionManager;
     public UnityAction sceneInitialized;
+
     public void Start()
     {
+        playerActionManager = Links.instance.playerActionManager;
+        playerControlManager = Links.instance.playerControlManager;
         InitializeScene();
     }
     public void InitializeScene()
@@ -22,10 +26,12 @@ public class GameSceneInitializer : MonoBehaviour
     }
     public void CreatePlayerCharacter()
     {
+        Debug.Log(Links.instance.locationSettings);
         GameObject playerCharacter = Instantiate(Humanoid, Links.instance.locationSettings.GetRandomSpawnPoint().position, Quaternion.identity);
         Links.instance.mainCamera.GetComponent<CameraFollowing>().SetObjectToFollow(playerCharacter);
         playerCharacter.GetComponent<CharacterContoller>().SetControlManager(playerControlManager);
         playerCharacter.GetComponent<HumanoidBattleSystem>().SetActionManager(playerActionManager);
+        playerCharacter.GetComponentInChildren<ItemsCollector>().SetActionManager(playerActionManager);
         Links.instance.playerCharacter = playerCharacter;
     }
 }
