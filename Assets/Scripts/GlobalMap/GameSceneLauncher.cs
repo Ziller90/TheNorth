d0e89b34824b97d29a2cfbbd68e953bbd22e61cc
Zdorovtsov Ilya <ilyazdorovtsov@gmail.com>
2022-12-04 15:23:05 +0300

    UItEvents plugin added

--- Assets/Scripts/GlobalMap/GameSceneLauncher.cs
+++ Assets/Scripts/GlobalMap/GameSceneLauncher.cs
@@ -3,17 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+public enum GameType
+{
+    Singleplayer,
+    DeathMatch
+}
 public class GameSceneLauncher : MonoBehaviour
 {
     static int locationToLoad;
+    static GameType locationToLoadGameType;
+    public static GameType LocationToLoadGameType => locationToLoadGameType;
     public static int LocationToLoad => locationToLoad;
     public void LoadGameSceneWithLocation(int locationIndex)
     {
+        locationToLoadGameType = GameType.Singleplayer;
         locationToLoad = locationIndex;
         SceneManager.LoadScene("GameScene");
     }
-    public void LoadLobbySceneWithLocation(int locationIndex)
+    public void LoadLobbySceneWithLocation(int locationIndex, GameType gameType)
     {
+        locationToLoadGameType = gameType;
         locationToLoad = locationIndex;
         SceneManager.LoadScene("LobbyScene");
     }
--- Assets/Scripts/ScenesManagement/GameSceneInitializer.cs
+++ Assets/Scripts/ScenesManagement/GameSceneInitializer.cs
@@ -2,11 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine.SceneManagement;
 
 
-public class GameSceneInitializer : MonoBehaviour
+public class GameSceneInitializer : MonoBehaviourPunCallbacks
 {
     [SerializeField] GameObject Humanoid;
+    [SerializeField] GameObject MultiplayerHumanoid;
 
     ControlManager playerControlManager;
     ActionManager playerActionManager;
@@ -26,12 +30,43 @@ public class GameSceneInitializer : MonoBehaviour
     }
     public void CreatePlayerCharacter()
     {
-        Debug.Log(Links.instance.locationSettings);
-        GameObject playerCharacter = Instantiate(Humanoid, Links.instance.locationSettings.GetRandomSpawnPoint().position, Quaternion.identity);
+        GameObject playerCharacter = new GameObject();
+        if (GameSceneLauncher.LocationToLoadGameType == GameType.Singleplayer)
+        {
+            playerCharacter = Instantiate(Humanoid, Links.instance.locationSettings.GetRandomSpawnPoint().position, Quaternion.identity);
+        }
+        else if (GameSceneLauncher.LocationToLoadGameType == GameType.DeathMatch)
+        {
+            GameObject newPlayer = PhotonNetwork.Instantiate(MultiplayerHumanoid.name, Links.instance.locationSettings.GetRandomSpawnPoint().position, Quaternion.identity, 0);
+            Debug.Log("Your ID in room is" + PhotonNetwork.LocalPlayer.ActorNumber);
+        }
         Links.instance.mainCamera.GetComponent<CameraFollowing>().SetObjectToFollow(playerCharacter);
         playerCharacter.GetComponent<CharacterContoller>().SetControlManager(playerControlManager);
         playerCharacter.GetComponent<HumanoidBattleSystem>().SetActionManager(playerActionManager);
         playerCharacter.GetComponentInChildren<ItemsCollector>().SetActionManager(playerActionManager);
         Links.instance.playerCharacter = playerCharacter;
+    }
+    public void LeaveRoom()
+    {
+        if (PhotonNetwork.IsMasterClient)
+        {
+            PhotonNetwork.LoadLevel("LobbyScene");
+        }
+        else
+        {
+            SceneManager.LoadScene("GlobalMapScene");
+        }
+        PhotonNetwork.LeaveRoom();
+    }
+    public override void OnPlayerEnteredRoom(Player other)
+    {
+        if (PhotonNetwork.IsMasterClient)
+        {
+            Debug.Log(other.NickName + " entered the room " + PhotonNetwork.CurrentRoom);
+        }
+    }
+    public override void OnPlayerLeftRoom(Player other)
+    {
+        Debug.Log(other.NickName + " left the room " + PhotonNetwork.CurrentRoom);
     }
 }
