a53bb9a5ba3e22956a550400100927381c434eee
Zdorovtsov Ilya <ilyazdorovtsov@gmail.com>
2023-02-03 21:05:45 +0300

    CameraFollowing script improved, raw models added

--- Assets/Scripts/CharacterControl/CameraFollowing.cs
+++ Assets/Scripts/CharacterControl/CameraFollowing.cs
@@ -5,18 +5,44 @@ using UnityEngine;
 public class CameraFollowing : MonoBehaviour
 {
     [HideInInspector] public float cameraYRotation;
-    [SerializeField] GameObject objectToFollow;
-    [SerializeField] Vector3 standartOffset;
-    [SerializeField] float zoom;
+    [SerializeField] Quaternion startRotation;
+    [SerializeField] Vector3 startOffset;
 
+    [SerializeField] GameObject objectToFollow;
+    [SerializeField] Vector3 currentOffset;
+    [SerializeField] Quaternion currentRotation;
+    [SerializeField] float zoom;
+    [SerializeField] bool follow;
+
+    private void Awake()
+    {
+        Debug.Log("Awake");
+        currentOffset = startOffset;
+        currentRotation = startRotation;
+    }
     void FixedUpdate()
     {
         cameraYRotation = gameObject.transform.rotation.eulerAngles.y;
-        gameObject.transform.position = objectToFollow.transform.position + standartOffset * zoom;
+        if (follow)
+        {
+            gameObject.transform.position = objectToFollow.transform.position + currentOffset * zoom;
+            gameObject.transform.rotation = currentRotation;
+        }
     }
     public void SetObjectToFollow(GameObject objectToFollow)
     {
         this.objectToFollow = objectToFollow;
+    }
+    //private void OnValidate()
+    //{
+    //    currentOffset = gameObject.transform.position - objectToFollow.transform.position;
+    //    currentRotation = gameObject.transform.rotation;
+    //}
+    [ContextMenu("SetCurrentOffsetAndRotation")]
+    public void SetCurrentOffsetAndRotation()
+    {
+        currentOffset = gameObject.transform.position - objectToFollow.transform.position;
+        currentRotation = gameObject.transform.rotation;
     }
 }
 
