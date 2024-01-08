49ecfc47c0eeeb8b4c0a80feab88944048523a0b
Zdorovtsov Ilya <ilyazdorovtsov@gmail.com>
2024-01-08 22:58:12 +0300

    Chase enemy and idle tasks were added

--- Assets/Scripts/Runtime/BehaviourTree/AIBehaviourTree.cs
+++ Assets/Scripts/Runtime/BehaviourTree/AIBehaviourTree.cs
@@ -6,12 +6,43 @@ using UnityEngine;
 [Serializable]
 public class AIBehaviourTree : MonoBehaviour
 {
+    [SerializeField] float searchingRadius;
+    [SerializeField] float enemySearchingTime;
+    [SerializeField] int searhingCornersNumber;
+    [SerializeField] float distanceToAttack;
+    [SerializeField] float distanceToLastEnemyPosition;
+    [SerializeField] GameObject debugPoint;
+
+    [Header("Patrolling")]
+    [SerializeField] Route patrolRoute;
+    [SerializeField] MovingMode patrolMovingMode;
+
+    [Header("Behaviour Tree:")]
     [SerializeReference, SubclassSelector] Node root;
 
-    public Dictionary<string, object> data = new Dictionary<string, object>();
+    public float SearchingRadius { get => searchingRadius; set => searchingRadius = value; }
+    public float EnemySearchingTime { get => enemySearchingTime; set => enemySearchingTime = value; }
+    public int SearhingCornersNumber { get => searhingCornersNumber; set => searhingCornersNumber = value; }
+    public float DistanceToAttack { get => distanceToAttack; set => distanceToAttack = value; }
+    public float DistanceToLastEnemyPosition { get => distanceToLastEnemyPosition; set => distanceToLastEnemyPosition = value; }
+    public GameObject DebugPoint { get => debugPoint; set => debugPoint = value; }
+    public Route PatrolRoute { get => patrolRoute; set => patrolRoute = value; }
+    public MovingMode PatrolMovingMode { get => patrolMovingMode; set => patrolMovingMode = value; }
+    public AINavigationManager AINavigationManager => navigationManager;
+    public Sensors Sensors => sensors;  
+    public Transform CurrentEnemy => sensors.CurrentEnemy;
+    public ActionManager ActionManager => actionManager;
+    public Vector3 LastEnemyPosition { get; set; }
+
+    AINavigationManager navigationManager;
+    ActionManager actionManager;
+    Sensors sensors;
 
     void Start()
     {
+        navigationManager = GetComponent<AINavigationManager>();
+        sensors = GetComponent<Sensors>();
+        actionManager = GetComponent<ActionManager>();
         root.Initialize(this, null);
     }
 
@@ -19,22 +50,5 @@ public class AIBehaviourTree : MonoBehaviour
     {
         if (root != null)
             root.Evaluate();
-    }
-
-    public void SetData(string key, object value)
-    {
-        data[key] = value;
-    }
-
-    public object GetData(string key)
-    {
-        data.TryGetValue(key, out object value);
-        return value;
-    }
-
-    public void ClearData(string key)
-    {
-        if (data.ContainsKey(key))
-            data.Remove(key);
     }
 }
