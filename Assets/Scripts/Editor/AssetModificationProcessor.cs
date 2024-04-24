7537c3415505077c504a81ce18ab5a6b15ac8cab
Ziller <ilyazdorovtsov@gmail.com>
2024-04-24 10:28:17 +0300

    ScriptableObjectManager implemented

---
+++ Assets/Scripts/Editor/AssetModificationProcessor.cs
@@ -0,0 +1,13 @@
+﻿using UnityEditor;
+
+internal class MyAllPostprocessor : AssetPostprocessor
+{
+    static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths)
+    {
+        if (importedAssets.Length > 0 || deletedAssets.Length > 0)
+        {
+            Game.ScriptableObjectManager?.UpdateList();
+            Game.PrefabManager?.UpdatePrefabManager();
+        }
+    }
+}
